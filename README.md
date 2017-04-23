19a253408e806c2ff73479584adc783ca07aa13c
Dmitry Fedotov <dmitry.fedotov@gmail.com>
2017-04-23 11:25:04 +0200

    -CGI -FPM +YAML ext
    Added yaml extension as a shared library, removed apache related stuff,
    disabled cgi and fpm - cli only

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Install php7 with pthreads on Ubuntu
+# Install php7 with pthreads on Ubuntu 16.04 LTS
 
 ###### en-US
-Install php7.0.8 with pthreads 3.1.6  on Ubuntu 16.04 LTS
+Install php7.0.8 CLI with pthreads 3.1.6 and yaml 2.0.0 on Ubuntu 16.04 LTS
 
 - Just put the file on your server, give permissions to execute him with chmod +x, and execute ./pthreads.sh
 - OBS: pthreads works only on CLI mode.
+- FPM is not enabled.
+- CGI is disabled.
 
 Thanks to @krakjoe
 
 ###### pt-BR
-Instalando php7.0.8 com pthreads 3.1.6 em um Ubuntu 16.04 LTS
+Instalando php7.0.8 CLI com pthreads 3.1.6 em um Ubuntu 16.04 LTS
 
 - Basta colocar o arquivo em seu servidor, dar permissões para executá-lo com chmod + x, e rodar o comando ./pthreads.sh
 - OBS: pthreads funcionam apenas no modo CLI.
--- pthreads.sh
+++ pthreads.sh
@@ -1,6 +1,10 @@
+#!/bin/sh
+
+# http://stackoverflow.com/questions/34969325/how-to-install-php7-zts-pthreads-on-ubuntu-14-04
+# https://github.com/ibrunotome/Install-php7-with-pthreads-on-ubuntu
+
 apt-get update -y
-apt-get install -y apache2 mysql-server
-apt-get install -y build-essential apache2-mpm-prefork apache2-prefork-dev libcurl4-openssl-dev libsqlite3-dev sqlite3 mysql-server libmysqlclient-dev libreadline-dev libzip-dev libxslt1-dev libicu-dev libmcrypt-dev libmhash-dev libpcre3-dev libjpeg-dev libpng12-dev libfreetype6-dev libbz2-dev libxpm-dev
+apt-get install -y build-essential libcurl4-openssl-dev libsqlite3-dev sqlite3 mysql-server libmysqlclient-dev libreadline-dev libzip-dev libxslt1-dev libicu-dev libmcrypt-dev libmhash-dev libpcre3-dev libjpeg-dev libpng12-dev libfreetype6-dev libbz2-dev libxpm-dev
 apt-get -y build-dep php7.0
 
 wget http://cl1.php.net/get/php-7.0.8.tar.gz/from/this/mirror -O php-7.0.8.tar.gz
@@ -14,6 +18,11 @@ tar zxvf pthreads-3.1.6.tgz
 
 cp -a pthreads-3.1.6/. php-7.0.8/ext/pthreads/
 
+wget http://pecl.php.net/get/yaml-2.0.0.tgz -O yaml-2.0.0.tgz
+tar zxvf yaml-2.0.0.tgz
+
+cp -a yaml-2.0.0/. php-7.0.8/ext/yaml/
+
 cd php-7.0.8
 
 rm -rf aclocal.m4
@@ -21,8 +30,12 @@ rm -rf autom4te.cache/
 ./buildconf --force
 make distclean
 
-./configure --disable-fileinfo --enable-maintainer-zts --enable-pthreads --prefix=/usr --with-config-file-path=/etc --with-curl --enable-cli --with-apxs2=/usr/bin/apxs2 \
---enable-mbstring \
+./configure --disable-fileinfo --enable-maintainer-zts --enable-pthreads --prefix=/etc/php7 \
+    --with-config-file-path=/etc/php7/cli --with-curl --enable-cli \
+    --with-config-file-scan-dir=/etc/php7/etc \
+    --disable-cgi \
+    --enable-debug \
+    --enable-mbstring \
     --enable-mbregex \
     --enable-phar \
     --enable-posix \
@@ -52,31 +65,38 @@ make distclean
     --with-openssl-dir=/usr/bin \
     --with-mysql-sock=/var/run/mysqld/mysqld.sock \
     --with-mysqli=mysqlnd \
-    --with-sqlite3=/usr \
+    --with-sqlite3 \
     --with-pdo-mysql=mysqlnd \
-    --with-pdo-sqlite=/usr \
-    --enable-fpm \
-    --with-fpm-user=www-data \
-    --with-fpm-group=www-data \
+    --with-pdo-sqlite \
     --config-cache \
     --localstatedir=/var \
     --with-layout=GNU \
     --disable-rpath
-    
+
 make clear 
 
 make
 
 make install
-cp php.ini-development /etc/php.ini
+#cp php.ini-development /etc/php7/cli/php.ini
+#cp php.ini-development /etc/php7/cli/php-cli.ini
 
-cp /etc/apache2/mods-available/php7.load /etc/apache2/mods-enabled/php7.load
-echo "<FilesMatch \.php$>
-    SetHandler application/x-httpd-php
-</FilesMatch>
-" > /etc/apache2/mods-enabled/php7.conf
+cp -r php.ini-production /etc/php7/cli/php.ini
+cp php.ini-production /etc/php7/cli/php-cli.ini
 
-/etc/init.d/apache2 restart
+cd ext/yaml
+
+/etc/php7/bin/phpize
+
+./configure --prefix='/etc/php7' --with-libdir='/lib/x86_64-linux-gnu' --with-php-config='/etc/php7/bin/php-config'
+
+make
+
+make install
+
+cd ../../
+
+echo "extension=yaml.so" >> /etc/php7/cli/php-cli.ini
 
 cd ..
 
@@ -84,12 +104,3 @@ rm php-7.0.8.tar.gz
 rm -rf php-7.0.8
 rm -rf pthreads-3.1.6
 rm pthreads-3.1.6.tgz
-
-sed -i "s/^;date.timezone =$/date.timezone = \"America\/Sao_Paulo\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
-
-apt-get install -y ntp ntpdate
-
-/etc/init.d/ntp stop 
-ntpdate ntp.shoa.cl
-/etc/init.d/ntp start
-date
